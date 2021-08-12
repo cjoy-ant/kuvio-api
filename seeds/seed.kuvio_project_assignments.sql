6b328ff348350a5e7a46822d4d6fbc857298a141
Christine Antonio <christinejm.antonio@gmail.com>
2021-08-12 15:43:27 -0700

    fix project assignment seed file

--- seeds/seed.kuvio_project_assignments.sql
+++ seeds/seed.kuvio_project_assignments.sql
@@ -1,4 +1,4 @@
-INSERT INTO kuvio_project_assignments (employee, project)
+INSERT INTO kuvio_project_assignments (project, employee)
 VALUES 
   ('93a80416-facb-11eb-9a03-0242ac130003', '076a6bc0-fa0c-11eb-9a03-0242ac130003'),
   ('93a80416-facb-11eb-9a03-0242ac130003', '100e737a-fa0c-11eb-9a03-0242ac130003'),
