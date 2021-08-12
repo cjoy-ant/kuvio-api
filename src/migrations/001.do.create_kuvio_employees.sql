5c4d3e5db695633c2fa4a933eac5afea15fb77aa
Christine Antonio <christinejm.antonio@gmail.com>
2021-08-12 13:25:11 -0700

    migrations 001

---
+++ src/migrations/001.do.create_kuvio_employees.sql
@@ -0,0 +1,10 @@
+CREATE EXTENSION IF NOT EXISTS "uuid-ossp";
+CREATE TABLE kuvio_employees (
+  emp_id UUID PRIMARY KEY DEFAULT uuid_generate_v1mc(),
+  emp_first_name TEXT NOT NULL,
+  emp_last_name TEXT NOT NULL,
+  emp_country TEXT NOT NULL,
+  emp_dob TEXT NOT NULL,
+  emp_age TEXT NOT NULL,
+  emp_date_modified TIMESTAMPTZ DEFAULT NOW()
+);
