8fce9ee5a7a1697b992e488283d8bfcf81ff985d
Christine Antonio <christinejm.antonio@gmail.com>
2021-08-12 15:24:25 -0700

    fix migration files, foreign key for employee country

--- migrations/002.do.create_kuvio_employees.sql
+++ migrations/002.do.create_kuvio_employees.sql
@@ -3,7 +3,8 @@ CREATE TABLE kuvio_employees (
   emp_id UUID PRIMARY KEY DEFAULT uuid_generate_v1mc(),
   first_name TEXT NOT NULL,
   last_name TEXT NOT NULL,
-  country TEXT NOT NULL,
+  country VARCHAR(3)
+    REFERENCES kuvio_countries(code3) ON DELETE CASCADE NOT NULL,
   dob TEXT NOT NULL,
   age TEXT NOT NULL,
   date_modified TIMESTAMPTZ DEFAULT NOW()
