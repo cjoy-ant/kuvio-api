b75af74be038764555f1cb2205b01e66a1c7c44b
Christine Antonio <christinejm.antonio@gmail.com>
2021-08-12 14:32:42 -0700

    seed files

--- src/migrations/001.do.create_kuvio_countries.sql
+++ src/migrations/001.do.create_kuvio_countries.sql
@@ -1,5 +1,6 @@
 CREATE TABLE kuvio_countries (
   country_code VARCHAR(2),
-  country_number VARCHAR(3),
+  country_code3 VARCHAR(3),
   country_name TEXT NOT NULL
+  country_number VARCHAR(3),
 );
---
+++ src/seeds/seed.kuvio_customers.sql
@@ -0,0 +1,6 @@
+INSERT INTO kuvio_customers
+VALUES
+  ('128a124c-facc-11eb-9a03-0242ac130003', 'Mickey Mouse', '123-456-7890', 'mickeymouse@fakemail.com', '2021-08-10T00:00'),
+  ('4abe94da-facc-11eb-9a03-0242ac130003', 'Donald Duck', '123-456-7890', 'donaldduck@fakemail.com', '2021-08-10T00:00'),
+  ('725356e8-facc-11eb-9a03-0242ac130003', 'Prince Ali', '123-456-7890', 'princeali@fakemail.com', '2021-08-10T00:00')
+;
