a4b2a2eec5b0cef09899df81aac98ce8996bd7f4
Christine Antonio <christinejm.antonio@gmail.com>
2021-08-12 15:42:43 -0700

    fix project seed file removed assigned employees

--- seeds/seed.kuvio_projects.sql
+++ seeds/seed.kuvio_projects.sql
@@ -1,4 +1,4 @@
-INSERT INTO kuvio_projects (project_id, title, project_description, customer, deadline, assigned_employees, complete, date_modified)
+INSERT INTO kuvio_projects (project_id, title, project_description, customer, deadline, complete, date_modified)
 VALUES
 ('93a80416-facb-11eb-9a03-0242ac130003', 'Test Project 1', 'Description of test project', '128a124c-facc-11eb-9a03-0242ac130003', '2021-10-31', false, '2021-08-10T00:00'),
 ('468b5e70-facc-11eb-9a03-0242ac130003', 'Test Project 2', 'Description of test project', '4abe94da-facc-11eb-9a03-0242ac130003', '2022-01-15', false, '2021-08-10T00:00'),
