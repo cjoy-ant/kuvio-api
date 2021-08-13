5a4be180bd1cd96268ded12accf1db4649883f54
Christine Antonio <christinejm.antonio@gmail.com>
2021-08-13 16:09:11 -0700

    add client origin to cors in app.js

--- src/app.js
+++ src/app.js
@@ -3,7 +3,7 @@ const express = require("express");
 const morgan = require("morgan");
 const helmet = require("helmet");
 const cors = require("cors");
-const { NODE_ENV } = require("./config");
+const { NODE_ENV, CLIENT_ORIGIN, LOCAL_CLIENT } = require("./config");
 const errorHandler = require("./error-handler");
 const employeesRouter = require("./employees/employees-router");
 const customersRouter = require("./customers/customers-router");
@@ -16,7 +16,11 @@ const morganOption = NODE_ENV === "production" ? "tiny" : "common";
 
 app.use(morgan(morganOption));
 app.use(helmet());
-app.use(cors());
+app.use(
+  cors({
+    origin: [CLIENT_ORIGIN, LOCAL_CLIENT],
+  })
+);
 
 app.use("/api/employees", employeesRouter);
 app.use("/api/customers", customersRouter);
